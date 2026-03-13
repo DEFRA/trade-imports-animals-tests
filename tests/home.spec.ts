52b6b863bd9a841ac817efe90388717cdb7c0667
Martyn Nevers (esynergy) <192345445+martyn-esynergy@users.noreply.github.com>
2026-03-13 14:40:18 +0000

    Revert "test(EUDPA-2): add intentional failing test"
    This reverts commit 527686a54ed563cef33cfb7c4f868afa795e29a0.

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -2,10 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('/');
-  await expect(page).toHaveTitle('Import notification service | Animals');
-});
-
-test('intentionally fails when expecting incorrect title', async ({ page }) => {
-  await page.goto('/');
-  await expect(page).toHaveTitle('Import notification service | Plants');
+  await expect(page).toHaveTitle('Home | Animals');
 });
