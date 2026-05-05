c51b4f9b35fd8cb65cefb341da2aa947175691a7
Martyn Nevers (esynergy) <192345445+martyn-esynergy@users.noreply.github.com>
2026-05-05 13:02:13 +0100

    test(a11y): increase per-test timeout for a11y scan journeys

--- playwright.a11y.config.ts
+++ playwright.a11y.config.ts
@@ -3,6 +3,8 @@ import baseConfig from './playwright.config';
 
 export default defineConfig({
   ...baseConfig,
+  /* A11y test timeout adjusted to 2m. */
+  timeout: 2 * 60 * 1000,
   retries: 0,
   use: {
     ...baseConfig.use,
