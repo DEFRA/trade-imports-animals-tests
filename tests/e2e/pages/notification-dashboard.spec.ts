b883542ec58396b16b140da60b7971a3c14110e3
TARUN KUMAR PALISETTY <tarun.mca@gmail.com>
2026-05-26 13:04:16 +0100

    EUDPA-69 dashboard with notification list (#41)
    * EUDPA-69 dashboard with notification list
    
    * Updated the notification reference to expect of format: GBN-AG...
    
    ---------
    
    Co-authored-by: tarun.kumar <tarun.kumar@defra.onmicrosoft.com>

--- tests/e2e/pages/notification-dashboard.spec.ts
+++ tests/e2e/pages/notification-dashboard.spec.ts
@@ -15,4 +15,25 @@ test.describe('Import notification service', () => {
     await expect(pages.page).toHaveURL(pages.originOfImport.expectedUrl);
     await expect(pages.originOfImport.heading).toBeVisible();
   });
+
+  test('displays the notification list on the home page', async ({ pages }) => {
+    await expect(pages.page).toHaveURL(pages.notificationDashboard.expectedUrl);
+    await expect(pages.notificationDashboard.heading).toBeVisible();
+    await expect(pages.notificationDashboard.totalResults).toBeVisible();
+    await expect(pages.notificationDashboard.totalResults).toHaveText(/\d+ Results/);
+  });
+
+  test('displays details on the first notification card', async ({ pages }) => {
+    const firstCard = pages.notificationDashboard.notificationCard(0);
+
+    await expect(firstCard.heading).toBeVisible();
+    await expect(firstCard.heading).toContainText(/GBN-AG-\d{2}-[A-Z0-9]+/);
+    await expect(firstCard.commodity).toBeVisible();
+    await expect(firstCard.origin).toBeVisible();
+    await expect(firstCard.arrivalAtDestination).toContainText(/\d{1,2} \w+ \d{4}/);
+    await expect(firstCard.status).toBeVisible();
+    await expect(firstCard.status).toContainText(/Draft|Submitted/);
+    await expect(firstCard.dateCreated).toBeVisible();
+    await expect(firstCard.dateCreated).toHaveText(/Date created: \d{1,2} \w+ \d{4}/);
+  });
 });
