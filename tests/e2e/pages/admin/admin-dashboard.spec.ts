5eb33b41d0873dd992856e7c0e074f837bba722c
hamid.jemei <hamid.jemei@defra.onmicrosoft.com>
2026-06-05 16:15:11 +0100

    moving page test to frontend service

--- tests/e2e/pages/admin/admin-dashboard.spec.ts
+++ tests/e2e/pages/admin/admin-dashboard.spec.ts
@@ -15,13 +15,4 @@ test.describe('Admin service', () => {
     await expect(pages.page).toHaveURL(pages.adminNotifications.expectedUrl);
     await expect(pages.adminNotifications.heading).toBeVisible();
   });
-
-  test(
-    'shows pagination on the notifications page when there are more than one page of results',
-    { tag: '@compose' },
-    async ({ pages }) => {
-      await pages.adminDashboard.btnNotifications.click();
-      await expect(pages.adminNotifications.pagination).toBeVisible();
-    },
-  );
 });
